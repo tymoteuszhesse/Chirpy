a966522f5a9d2a12032bb33b729f59173f60e906
tymekhesse <tymek@ozoneproject.com>
2025-07-24 15:11:05 +0200

    Serving Images

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 func main() {
 	const port = "8080"
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/assets/logo", http.FileServer(http.Dir("./assets/")))
 	server := http.Server{Handler: mux, Addr: ":" + port}
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(server.ListenAndServe())
