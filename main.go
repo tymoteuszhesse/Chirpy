01802f606dafcc43e9f0fa2a38a308384dd0e0eb
tymekhesse <tymek@ozoneproject.com>
2025-07-27 02:01:31 +0200

    Stateful Handlers

--- main.go
+++ main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(res, req)
+	})
+}
+func (cfg *apiConfig) countHandler(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) resetHandler(res http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
+
+func (cfg *apiConfig) ServeHttp(res http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Add(1)
+}
 
 func main() {
 	const port = "8080"
+	apiCfg := apiConfig{}
 	mux := http.NewServeMux()
 	handler := http.FileServer(http.Dir("."))
-	mux.Handle("/app/", http.StripPrefix("/app", handler))
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(handler)))
+
+	mux.HandleFunc("/metrics", apiCfg.countHandler)
+	mux.HandleFunc("/reset", apiCfg.resetHandler)
+
 	mux.HandleFunc("/healthz", func(res http.ResponseWriter, req *http.Request) {
 		res.Header().Add("Content-Type", "text/plain; charset=utf-8")
 		res.WriteHeader(200)
