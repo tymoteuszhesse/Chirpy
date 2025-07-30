ae8aa9c155c921b1c7e5e89e1d7b96bfb2ea61a9
tymekhesse <tymek@ozoneproject.com>
2025-07-30 22:12:10 +0200

    The Profane

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"sync/atomic"
 )
 
@@ -45,7 +47,79 @@ func main() {
 		res.WriteHeader(200)
 		res.Write([]byte("OK"))
 	})
+
+	mux.HandleFunc("POST /api/validate_chirp", func(res http.ResponseWriter, req *http.Request) {
+		type bodyReq struct {
+			Body string `json:"body"`
+		}
+
+		type cleanedBody struct {
+			CleanedBody string `json:"cleaned_body"`
+		}
+		decoder := json.NewDecoder(req.Body)
+		body := bodyReq{}
+		err := decoder.Decode(&body)
+		if err != nil {
+			log.Printf("Error decoding parameters: %s", err)
+			res.WriteHeader(500)
+			return
+		}
+		if len(body.Body) > 140 {
+			respondWithError(res, 400, "Chirp is too long")
+		}
+
+		body.Body = cleanBadWords(body.Body)
+
+		cleanedResp := cleanedBody{
+			CleanedBody: body.Body,
+		}
+		resp, err := json.Marshal(cleanedResp)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			res.WriteHeader(500)
+			return
+		}
+		respondWithJSON(res, 200, resp)
+	})
+
 	server := http.Server{Handler: mux, Addr: ":" + port}
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func cleanBadWords(text string) string {
+	words := strings.Split(text, " ")
+	bannedWords := []string{"kerfuffle", "sharbert", "fornax"}
+	for _, word := range words {
+		for _, bannedWord := range bannedWords {
+			if strings.ToLower(word) == bannedWord {
+				text = strings.ReplaceAll(text, word, "****")
+			}
+		}
+	}
+	return text
+}
+
+func respondWithError(res http.ResponseWriter, code int, msg string) {
+	type errResp struct {
+		Error string `json:"error"`
+	}
+	respBody := errResp{
+		Error: msg,
+	}
+	resp, err := json.Marshal(respBody)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		res.WriteHeader(500)
+		return
+	}
+	res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(code)
+	res.Write(resp)
+}
+
+func respondWithJSON(res http.ResponseWriter, code int, payload interface{}) {
+	res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(code)
+	res.Write(payload.([]byte))
 }
